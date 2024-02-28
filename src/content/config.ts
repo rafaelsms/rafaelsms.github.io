bf3adee5ad207c5ac154d9dc613e0dc14ab067f4
Rafael Sartori M. Santos <eu@rafaelsms.com>
2024-02-28 00:59:24 -0300

    Updates astro, adds a photo grid with categories

---
+++ src/content/config.ts
@@ -0,0 +1,25 @@
+import { z, defineCollection, reference } from 'astro:content';
+
+const photoCategories = defineCollection({
+	type: 'data',
+	schema: ({ image }) => z.object({
+		title: z.string(),
+		imageDescription: z.string(),
+		coverImage: image()
+	})
+});
+
+const photos = defineCollection({
+	type: 'data',
+	schema: ({ image }) => z.object({
+		title: z.string(),
+		description: z.string(),
+		category: reference('photo_categories'),
+		image: image()
+	})
+});
+
+export const collections = {
+	photo_categories: photoCategories,
+	photos: photos
+};
